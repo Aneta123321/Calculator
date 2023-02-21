29c2d330113b00f9228da1d17011547d6e647eca
Aneta G <aneta.golembiecka@gmail.com>
2023-02-21 18:48:36 +0100

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+
+    }
+
+    public double minus(double a, double b) {
+        return a - b;
+    }
+}
