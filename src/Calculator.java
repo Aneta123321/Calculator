c239b6323d0a0a4dd90e0580e7defca91eadbae6
Aneta G <aneta.golembiecka@gmail.com>
2023-02-21 18:57:39 +0100

    Second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,14 @@ public class Calculator {
     public double minus(double a, double b) {
         return a - b;
     }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        double result1 = calculator.add(5.10,4.90);
+        System.out.println(result1);
+
+        double result2 = calculator.minus(5.10,4.90);
+        System.out.println(result2);
+
+    }
 }
